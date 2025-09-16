18f76b44e9d1ea19b0cb796e120a82b8076de201
nicosikmoraes <nicolas.moraes.ns@gmail.com>
2025-09-16 19:35:23 -0300

    Main Netflix

---
+++ styles/components/Main/ImageShow.tsx
@@ -0,0 +1,19 @@
+import {Image, StyleSheet, Text, View  } from 'react-native'
+import React from 'react'
+
+type imageProps = {
+    image_url: string;
+}
+
+export default function ImageShow({image_url}: imageProps) {
+  return (
+    <Image
+    style={{width: 100, height: 140}}
+        source={{
+            uri: image_url
+        }}
+    />
+  )
+}
+
+const styles = StyleSheet.create({})
--- styles/components/Main/index.tsx
+++ styles/components/Main/index.tsx
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import New from './New'
+import Sports from './Sports'
 
 export default function Main() {
   return (
     <View style={styles.main_container}>
-      <Text style={styles.main_text}>Main</Text>
+      <New />
+
+      <Sports />
     </View>
   )
 }
@@ -12,6 +16,8 @@ export default function Main() {
 const styles = StyleSheet.create({
   main_container: {
     display: 'flex',
+    flexDirection: 'column',
+    gap: 20
   },
 
   main_text: {

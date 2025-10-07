86cbdeb6802232c444e3baf5c46334f7f24a7f7f
NicoSikMoraes <nicolas.moraes.ns@gmail.com>
2025-10-07 17:31:30 -0300

    avaliação 1

--- adota_parana/app/index.tsx
+++ adota_parana/app/index.tsx
@@ -2,15 +2,12 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Home from "@/components/Home";
-import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 
 export default function index() {
   return (
-    <ActionSheetProvider>
-      <ScreenWrapper>
-        <Home />
-      </ScreenWrapper>
-    </ActionSheetProvider>
+    <ScreenWrapper>
+      <Home />
+    </ScreenWrapper>
   );
 }
 
